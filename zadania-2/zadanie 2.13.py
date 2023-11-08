68ad0ea3dbb8cdd1f36eabd6823dcb605e341f38
poulCuzz <pawel1818@outlook.com>
2023-11-08 21:29:28 +0100

    poprawione 2.13

--- zadania-2/zadanie 2.13.py
+++ zadania-2/zadanie 2.13.py
@@ -5,7 +5,8 @@
 # utwórz program, który zamieni cyfrę rzymską na arabską (do 1000).
 
 romanArabic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-romanNum = input("podaj liczbe rzymska: ")
+romanNum = input("podaj liczbe rzymska: ").upper()
+
 
 numList = []
 for i in range(0, len(romanNum)):
