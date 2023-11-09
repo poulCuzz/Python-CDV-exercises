214e84508f5267ceb0e06a1a22969f3d4b7bb394
poulCuzz <pawel1818@outlook.com>
2023-11-09 14:36:20 +0100

    poprawione 2.13

--- zadania-2/zadanie 2.15.py
+++ zadania-2/zadanie 2.15.py
@@ -1,18 +1,7 @@
-while True:
-    num = input("Wprowadz 5 cyfr: ")
-    if num.isdigit() and len(num) == 5:
-        break
-    else:
-        print("blad! Wprowadz poprawna liczbe")
-
-
-numDictionary = {1: "jeden", 2: "dwa", 3: "trzy", 4: "cztery", 5: "piec", 6: "szesc", 7: "siedem", 8: "osiem", 9: "dziewiec", 0: "zero"}
-# for i in range(0, len(num)):
-#     print(numDictionary.get(int(num[i])))
-myList = []
-for i in range(0, len(num)):
-    myList.append(numDictionary.get(int(num[i])))
-
-print(num + " - " + myList[0] + " " + myList[1] + " " + myList[2] + " " + myList[3] + " " + myList[4])
-
-
+slownik = {"m1": "Ford", "m2": "Fiat", "m3": "Opel", "m4": "Skoda", "m5": "Wołga", "m6": "Dacia"}
+copyslownik = slownik.copy()
+slownik.items()
+update = {"m7": "FSO"}
+slownik.update(update)
+slownik.clear()
+print(slownik)
