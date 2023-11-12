49624658a048d1869514ac65d66ce3d6e14ec2c5
poulCuzz <pawel1818@outlook.com>
2023-11-12 18:09:44 +0100

    Tic Tac Toe - koniec pracy nad najprostrzą wersją; komputer robi losowe ruchy. Gra nie jest dobrze zabezpieczona na błedy

--- zadania-2/Pętle/Tic_Tac_Toe.py
+++ zadania-2/Pętle/Tic_Tac_Toe.py
@@ -1,36 +1,75 @@
-#Tic Tac Toe
+# Tic Tac Toe
+
+# Napisz grę w kółko i krzyżyk człowieka z komputerem. W grze obowiązują klasyczne zasady. Układ planszy powinien być wymiaru 3x3.
 import random
-#Napisz grę w kółko i krzyżyk człowieka z komputerem. W grze obowiązują klasyczne zasady. Układ planszy powinien być wymiaru 3x3.
 import time
 
 print("Welcome in Tic Tac Toe!!!")
 time.sleep(1)
 print("Here is our game board:")
 time.sleep(1)
+print("")
 print("    a    b    c")
-time.sleep(0.3)
 print("1 [   ][   ][   ]")
-time.sleep(0.3)
 print("2 [   ][   ][   ]")
-time.sleep(0.3)
 print("3 [   ][   ][   ]")
+print("")
 time.sleep(1.5)
 print("Select the position to insert 'x' by entering the coordinate")
-time.sleep(1.8)
 print("Let's start!")
 print("")
 coordinList = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"]
 mapo = {}
+
+#funkcja do weryfikowania zwycięzcy/przegranego
+def verifyWinner(board):
+    try:
+        # Sprawdzenie rzędów
+        for row in '123':
+            if board[f'a{row}'] == board[f'b{row}'] == board[f'c{row}'] and board[f'a{row}'] != ' ':
+                return board[f'a{row}']
+    except Exception as e:
+        print("")
+
+    try:
+        # Sprawdzenie kolumn
+        for column in 'abc':
+            if board[f'{column}1'] == board[f'{column}2'] == board[f'{column}3'] and board[f'{column}1'] != ' ':
+                return board[f'{column}1']
+    except Exception as e:
+        print("")
+
+    try:
+        # Sprawdzenie przekątnych
+        if board['a1'] == board['b2'] == board['c3'] and board['a1'] != ' ':
+            return board['a1']
+        if board['a3'] == board['b2'] == board['c1'] and board['a3'] != ' ':
+            return board['a3']
+    except Exception as e:
+        print("")
+
+        return None
+
+
+
+#Pętla do wstawiania 'x' przez użytkownika i 'o' przez komputer
 while len(coordinList) > 0:
 
     coordinate = input("Your move: ")
     coordinList.remove(coordinate)
     mapo[coordinate] = "x"
-    # print(coordinList)
+
+    # Wywołanie funkcji
+    result = verifyWinner(mapo)
+    if result == "x":
+        break
+    elif result == "o":
+        break
+
+
     if len(coordinList) != 0:
         print("PC's turn:")
         pcCoordin = coordinList[random.randint(0, len(coordinList) - 1)]
-        print(pcCoordin)
         coordinList.remove(pcCoordin)
         mapo[pcCoordin] = "o"
     print("    a    b    c")
@@ -38,7 +77,12 @@ while len(coordinList) > 0:
     print(f"2 [ {mapo.get('a2', ' ')} ][ {mapo.get('b2', ' ')} ][ {mapo.get('c2', ' ')} ]")
     print(f"3 [ {mapo.get('a3', ' ')} ][ {mapo.get('b3', ' ')} ][ {mapo.get('c3', ' ')} ]")
 
-
+    # Wywołanie funkcji
+    result = verifyWinner(mapo)
+    if result == "x":
+        break
+    elif result == "o":
+        break
 
 
 print("    a    b    c")
@@ -46,8 +90,11 @@ print(f"1 [ {mapo.get('a1', ' ')} ][ {mapo.get('b1', ' ')} ][ {mapo.get('c1', ' 
 print(f"2 [ {mapo.get('a2', ' ')} ][ {mapo.get('b2', ' ')} ][ {mapo.get('c2', ' ')} ]")
 print(f"3 [ {mapo.get('a3', ' ')} ][ {mapo.get('b3', ' ')} ][ {mapo.get('c3', ' ')} ]")
 
-
-
-
-
-
+# Wywołanie funkcji i wyświetlenie zwycięzcy
+result = verifyWinner(mapo)
+if result == "x":
+    print("Gratulacje! Wygrałeś!")
+elif result == "o":
+    print("Przegrałeś :(")
+else:
+    print("Brak zwycięzcy.")
